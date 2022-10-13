cfdcf94cb7e927eeeeabd6edcfc1e17ad0a0b733
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-13 16:47:35 -0600

    enable Page.getLayout on _app

--- pokemon-static-app/src/pages/_app.tsx
+++ pokemon-static-app/src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CustomNextPage } from '../types/next/custom-next-page.type';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type ExtraAppProps = {
+  Component: CustomNextPage;
+};
+
+type CustomAppProps = AppProps & ExtraAppProps;
+
+function MyApp({ Component, pageProps }: CustomAppProps) {
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
 }
 
-export default MyApp
+export default MyApp;
