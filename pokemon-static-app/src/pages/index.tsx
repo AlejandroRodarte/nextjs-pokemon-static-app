e24b8b5f920d4df53e86b96219ef6974cd44b29d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-13 21:50:34 -0600

    45. Next - GetStaticProps

--- pokemon-static-app/src/pages/index.tsx
+++ pokemon-static-app/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@nextui-org/react';
+import { GetStaticProps } from 'next';
 import { DefaultLayout } from '../components/layouts';
 
 import { CustomNextPage } from '../types/next/custom-next-page.type';
@@ -7,14 +7,27 @@ interface HomePageProps {}
 
 const HomePage: CustomNextPage<HomePageProps> = () => {
   return (
-    <>
-      <Button color="gradient">Hola Mundo</Button>
-    </>
+    <ul>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+      <li>Pokemon</li>
+    </ul>
   );
 };
 
 HomePage.getLayout = (page) => (
   <DefaultLayout title="Listado de Pokemons">{page}</DefaultLayout>
 );
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async (
+  context
+) => {
+  return { props: {} };
+};
 
 export default HomePage;
