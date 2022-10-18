310b6b8177a59b59f5f2697e29ff2f967531b6d2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-18 15:26:39 -0600

    54. Diseño de la página de Pokémon

--- pokemon-static-app/src/interfaces/props/pokemon-full-info-data.interface.ts
+++ pokemon-static-app/src/interfaces/props/pokemon-full-info-data.interface.ts
@@ -1,1 +1,14 @@
-export interface PokemonFullInfoData {}
+export interface PokemonFullInfoData {
+  name: string;
+  sprites: {
+    back_default: string;
+    back_shiny: string;
+    front_default: string;
+    front_shiny: string;
+    other: {
+      dream_world: {
+        front_default?: string;
+      };
+    };
+  };
+}
--- pokemon-static-app/src/pages/pokemon/[id]/index.tsx
+++ pokemon-static-app/src/pages/pokemon/[id]/index.tsx
@@ -1,11 +1,13 @@
-import { GetStaticPaths, GetStaticPathsResult, GetStaticProps } from 'next';
 import React from 'react';
+import { GetStaticPaths, GetStaticPathsResult, GetStaticProps } from 'next';
+import { Button, Card, Container, Grid, Text } from '@nextui-org/react';
 
 import { DefaultLayout } from '../../../components/layouts';
 import { env } from '../../../config/env.config';
 import { CustomNextPage } from '../../../types/next/custom-next-page.type';
 import { pokemonService } from '../../../services/server/pokemon.service';
 import { PokemonFullInfoData } from '../../../interfaces/props/pokemon-full-info-data.interface';
+import Image from 'next/image';
 
 type PokemonPageUrlQuery = {
   id: string;
@@ -17,7 +19,73 @@ interface PokemonPageProps {
 
 const PokemonPage: CustomNextPage<PokemonPageProps> = (props) => {
   const { pokemon } = props;
-  return <div>PokemonPage</div>;
+  return (
+    <Grid.Container css={{ marginTop: '5px' }} gap={2}>
+      <Grid xs={12} sm={4}>
+        <Card isHoverable css={{ padding: '30px' }}>
+          <Card.Body>
+            <Card.Image
+              src={
+                pokemon.sprites.other.dream_world.front_default ||
+                '/no-image.png'
+              }
+              alt={pokemon.name}
+              width="100%"
+              height={200}
+            />
+          </Card.Body>
+        </Card>
+      </Grid>
+      <Grid xs={12} sm={8}>
+        <Card>
+          <Card.Header
+            css={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <Text h1 transform="capitalize">
+              {pokemon.name}
+            </Text>
+            <Button color="gradient" ghost>
+              Guardar en Favoritos
+            </Button>
+          </Card.Header>
+          <Card.Body>
+            <Text size={30}>Sprites:</Text>
+            <Container
+              direction="row"
+              display="flex"
+              gap={0}
+              justify="space-between"
+            >
+              <Image
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+                width={100}
+                height={100}
+              />
+              <Image
+                src={pokemon.sprites.back_default}
+                alt={pokemon.name}
+                width={100}
+                height={100}
+              />
+              <Image
+                src={pokemon.sprites.front_shiny}
+                alt={pokemon.name}
+                width={100}
+                height={100}
+              />
+              <Image
+                src={pokemon.sprites.back_shiny}
+                alt={pokemon.name}
+                width={100}
+                height={100}
+              />
+            </Container>
+          </Card.Body>
+        </Card>
+      </Grid>
+    </Grid.Container>
+  );
 };
 
 PokemonPage.getLayout = (page) => (
@@ -54,8 +122,23 @@ export const getStaticProps: GetStaticProps<
   const [response, error] = await pokemonService.getPokemon(id);
   if (error || !response) return { notFound: true };
 
+  const pokemon: PokemonFullInfoData = {
+    name: response.name,
+    sprites: {
+      back_default: response.sprites.back_default,
+      back_shiny: response.sprites.back_shiny,
+      front_default: response.sprites.front_default,
+      front_shiny: response.sprites.front_shiny,
+      other: {
+        dream_world: {
+          front_default: response.sprites.other?.dream_world.front_default,
+        },
+      },
+    },
+  };
+
   return {
-    props: { pokemon: {} },
+    props: { pokemon },
   };
 };
 
