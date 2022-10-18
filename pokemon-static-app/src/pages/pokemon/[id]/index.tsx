fa612b72c37c687570545006e70f185b9ec4a7e9
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-18 14:28:07 -0600

    52. Next - getStaticPaths

--- pokemon-static-app/src/pages/pokemon/[id]/index.tsx
+++ pokemon-static-app/src/pages/pokemon/[id]/index.tsx
@@ -1,9 +1,17 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
 import React from 'react';
 
 import { DefaultLayout } from '../../../components/layouts';
 import { CustomNextPage } from '../../../types/next/custom-next-page.type';
 
-interface PokemonPageProps {}
+type PokemonPageUrlQuery = {
+  id: string;
+};
+
+interface PokemonPageProps {
+  id: number;
+  name: string;
+}
 
 const PokemonPage: CustomNextPage<PokemonPageProps> = () => {
   return <div>PokemonPage</div>;
@@ -12,5 +20,29 @@ const PokemonPage: CustomNextPage<PokemonPageProps> = () => {
 PokemonPage.getLayout = (page) => (
   <DefaultLayout title="Algun pokemon">{page}</DefaultLayout>
 );
+
+export const getStaticPaths: GetStaticPaths<PokemonPageUrlQuery> = (
+  context
+) => {
+  return {
+    paths: [
+      {
+        params: {
+          id: '1',
+        },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PokemonPageProps> = (context) => {
+  return {
+    props: {
+      id: 1,
+      name: 'Bulbasaur',
+    },
+  };
+};
 
 export default PokemonPage;
